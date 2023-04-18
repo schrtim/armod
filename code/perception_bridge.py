0838f079803452259b8b5c015534903f45f4c634
Tim Schreiter <tim.schreiter@oru.se>
2023-04-18 15:19:45 +0200

    updated pointing and looking limitations

--- code/perception_bridge.py
+++ code/perception_bridge.py
@@ -5,37 +5,61 @@ import rospy
 from std_msgs.msg import String
 
 class ArmodCommand:
+    """A class that publishes commands to the Armod robot based on user input and perception data."""
+
     def __init__(self):
-        rospy.init_node('armod_command_publisher', anonymous=True)
-        self.pub = rospy.Publisher('armod_command', String, queue_size=10)
-        self.sub_keypress = rospy.Subscriber('face_detections', String, self.receive_perception_message)
-        self.pd = []
+        """Initialize the node, publisher, subscriber and perception data."""
+        # Create a ROS node with a unique name
+        rospy.init_node('armod_command_publisher', anonymous=True) 
+
+        # Create a publisher  with std_msg type for the armod_command topic 
+        # Std_msg are important here as they have to be send between different ROS distors
+        self.pub = rospy.Publisher('armod_command', String, queue_size=10) 
+
+        # Create a subscriber for the perception topics
+        self.sub_keypress = rospy.Subscriber('face_detections', String, self.receive_perception_message) 
+
+        # Initialize an empty list for storing the perception data
+        # Dictionary also possible if working with detection IDs
+        self.pd = [] 
 
     def send_command(self, command):
-        self.pub.publish(command)
+        """Publish a command to the armod_command topic as a string"""
+        # If the code is modifed this could be any std_msg
+        self.pub.publish(command) 
 
     def receive_perception_message(self, data):
-        bx, by, bz = data.data.split(",")
-        self.pd = [float(bx), float(by), float(bz)/1000]
+        """Receive and process the perception data from the face_detections topic."""
+
+        # Split the data into x, y and z coordinates
+        bx, by, bz = data.data.split(",") 
+        # Convert the coordinates to floats and store them in the pd list
+        self.pd = [float(bx), float(by), float(bz)/1000] 
         
 
     def run(self):
-        while not rospy.is_shutdown():
-            key = input("Press a key: ")
-            if key == 'p':
-                self.send_command("point,"+str(self.pd[0])+","+str(self.pd[1])+","+str(self.pd[2]))
-                print(self.pd)
-            elif key == 'l':
-                self.send_command("look,"+str(self.pd[0])+","+str(self.pd[1])+","+str(self.pd[2]))
-                print(self.pd)
-            elif key == 'q':
-                self.send_command("quit,"+str(0)+","+str(0)+","+str(0))
-                break
-            elif key == 'n':
-                self.send_command("nod,"+str(self.pd[0])+","+str(self.pd[1])+","+str(self.pd[2]))
-            else:
-                self.send_command(key)
+        """Run the main loop of the node.
+        
+        React on perceptions/detections and send commands to NAO
+        TODO: Adapt to DARKO Perception Stack
+        """
+
+        while not rospy.is_shutdown(): # loop until the node is shut down
+            key = input("Press a key: ") # get user input from keyboard
+            if key == 'p': # if the user presses p
+                self.send_command("point,"+str(self.pd[0])+","+str(self.pd[1])+","+str(self.pd[2])) # send a point command with the perception data
+                print(self.pd) # print the perception data
+            elif key == 'l': # if the user presses l
+                self.send_command("look,"+str(self.pd[0])+","+str(self.pd[1])+","+str(self.pd[2])) # send a look command with the perception data
+                print(self.pd) # print the perception data
+            elif key == 'q': # if the user presses q
+                self.send_command("quit,"+str(0)+","+str(0)+","+str(0)) # send a quit command with zero coordinates
+                break # exit the loop
+            elif key == 'n': # if the user presses n
+                self.send_command("nod,"+str(self.pd[0])+","+str(self.pd[1])+","+str(self.pd[2])) # send a nod command with the perception data
+            else: # if the user presses any other key
+                self.send_command(key) # send the key as a command
 
 if __name__ == '__main__':
-    armod_command = ArmodCommand()
-    armod_command.run()
+    armod_command = ArmodCommand() # create an instance of the ArmodCommand class
+    armod_command.run() # run the main loop of the node
--- code/robot_controller.py
+++ code/robot_controller.py
@@ -11,6 +11,7 @@ import os
 import sys
 import numpy as np
 import time
+import math
 
 # Import ROS, use only std_msg here, as they can be sent between distributions
 import rospy
@@ -41,10 +42,18 @@ CommandExecuter = None
 memory = None
 
 class CommandExecuterModule(ALModule):
-    """Counts seen Faces"""
+    """
+    This class represents a module for executing commands on a NAO robot.
+    It inherits from the ALModule class.
+    http://doc.aldebaran.com/2-8/naoqi/core/almodule.html
+    """
 
     def __init__(self,name):
+        """
+        Initializes a new instance of the CommandExecuterModule class.
 
+        :param name: The name of the module.
+        """
         global config
         # Change for head turning speed:
         self.maxSpeed = float(config['Timing']['NaoMaxSpeed'])
@@ -80,42 +89,69 @@ class CommandExecuterModule(ALModule):
         # Create Proxy ALtts for letting NAO speak
         self.tts = ALProxy("ALTextToSpeech") # No IP Adress or ports needed
         self.posture = ALProxy("ALRobotPosture")
+
         # The tracker module is for coordinate related movements
         self.tracker = ALProxy( "ALTracker" )
+
         # Enbales to run pre-installed behaviours
         self.bm = ALProxy("ALBehaviorManager")
+
         # Enables Playback of soundfiles
         self.framemanager = ALProxy("ALFrameManager")
-        self.player = ALProxy('ALAudioPlayer')
-        self.playerStop = ALProxy('ALAudioPlayer', True) #Create another proxy as wait is blocking if audioout is remote
 
+        # Audio player for playing sound files        
+        self.player = ALProxy('ALAudioPlayer')
+
+        # Create another proxy as wait is blocking if audioout is remote
+        self.playerStop = ALProxy('ALAudioPlayer', True)
+
+        # LED controller for controlling the LEDs on the robot
         self.led_controller = ALProxy("ALLeds")
-        self.flash_duration = 2 # Adjustable
-        self.delay_between_trials = 1 # Setbased on Lucas preferences
+
+         # Duration of LED flash in seconds (adjustable)
+        self.flash_duration = 2
+
+        # Delay between trials of an experiment
+        self.delay_between_trials = 1
 
         global memory
+        # Create a global memory proxy to access the robot's memory
         memory = ALProxy("ALMemory")
 
-        # Set a starting position for the robot here if desired
-
-        # self.posture.goToPosture("Crouch", 0.8)
-
-        # self.al = ALProxy("ALAutonomousLife")
-        # self.al.setState("disabled")
-        # self.somnus = ALProxy("ALMotion")
-        # self.somnus.wakeUp()
-
+        # Set the robot's posture to "Sit" with a speed of 0.8
         self.posture.goToPosture("Sit", 0.8)
-        #time.sleep(3)
 
-
-        # ROS Pubs n subs:
+        # ROS Publisher and subscriber:
+        # Subscribe to the 'armod_command' topic to receive commands
         self.sub_keypress = rospy.Subscriber('armod_command', String, self.ros_event_callback)
+        
+        # Initialize a ROS node with the name 'armod_controll'
         rospy.init_node('armod_controll', anonymous=True)
 
-        # self.sub_keypress = rospy.Subscriber('keypress', String, self.keypressCb)
-        # rospy.init_node('nao_cueing', anonymous=True)
+        # Disable Autonomous Living: ROBOT WILL STAND UP!
+        # Set the robot's posture to "Crouch" with a speed of 0.8
+        # self.posture.goToPosture("Crouch", 0.8)
+        
+        # Create a proxy for the ALAutonomousLife module
+        # self.al = ALProxy("ALAutonomousLife")
+        
+        # Disable the autonomous life state
+        # self.al.setState("disabled")
+        
+        # Create a proxy for the ALMotion module
+        # self.somnus = ALProxy("ALMotion")
+        
+        # Wake up the robot
+        # self.somnus.wakeUp()       
+
     def command_split(self, cmd):
+        """
+        Splits a received command string into its components.
+
+        :param cmd: The command string to split.
+        :return: A tuple containing the command and its arguments.
+        """
+
         split = cmd.split(",")
         if len(split)>1:
             command = split[0]
@@ -126,7 +162,11 @@ class CommandExecuterModule(ALModule):
         else: return split[0], 0, 0, 0
     
     def ros_event_callback(self, data):
+        """
+        Callback function for handling ROS events.
 
+        :param data: The data received from the ROS event.
+        """
         command, x, y, z = self.command_split(data.data)
 
         if command == "point":
@@ -148,6 +188,7 @@ class CommandExecuterModule(ALModule):
         elif command == "nod":
             self.updateCoordinates(x, -y, z)
             self.onCallLook()
+            # Uncomment the following line to flash the robot's eyes green
             # self.flash_eyes("green")
             self.onAffirmNod()
             time.sleep(3)
@@ -155,176 +196,108 @@ class CommandExecuterModule(ALModule):
         else:
             print(data.data)            
  
-    def wait_for_detection_or_intervall(self):
-        """
-            This function will process the given timestamp of when NAO turned the head.
-            Wait time can be adopted by changing the loop boundaries.
-            Currently its 100*0.02s = 2s
-            in:
-            - Naos timestamp in datetime format
-            return: None
-            Function will wait for 2s or until key is pressed.            
-        """
-        pass
-
     def detection_event(self, data):
+        # TODO Implement during Integration week
         pass
 
-    def updateCoordinates(self,x,y,z):
-        """Function that simply updates Parameters"""
+    def updateCoordinates(self, x, y, z):
+        """
+        Updates the coordinates of the robot.
+
+        :param x: The new x-coordinate.
+        :param y: The new y-coordinate.
+        :param z: The new z-coordinate.
+        """
         self.x = x
         self.y = y
         self.z = z
         self.resting = False
 
+    def check_FOV_limits(self, vector):
+        """
+        This function checks if a vector is within the FOV of a robot modelled after human FOV
+        The rest position of the robot's head is modulated with the vector [x=1, y=0, z=0].
+        The FOV is maximal 200 degrees horizontally and 150 degrees vertically.
+
+        Parameters:
+        vector (list): a list of three numbers representing the x, y, and z components of the vector.
+
+        Returns:
+        None: prints "Feasible vector" or "Not a feasible vector" depending on the angle between the vector and the rest position vector.
+
+        Explanation:
+        The angle between the vector and the rest position vector is the same as the angle between the vector and the x-axis.
+        This angle can be used to check both the horizontal and vertical FOV ranges.
+        For example, if the angle is 90 degrees, then the vector is perpendicular to the x-axis and lies on the yz-plane.
+        This means that it is at the edge of the horizontal FOV range, but it could be anywhere in the vertical FOV range.
+        If the angle is 45 degrees, then the vector is halfway between the x-axis and the yz-plane.
+        This means that it is within both the horizontal and vertical FOV ranges.
+        If the angle is 120 degrees, then the vector is opposite to the x-axis and lies on the negative yz-plane.
+        This means that it is outside both the horizontal and vertical FOV ranges.
+        """
+        
+        # calculate the angle between the vector and the rest position vector
+        # rest postion vector per default x=1, y=0, z=0
+        dot_product = vector[0] * 1 + vector[1] * 0 + vector[2] * 0
+        vector_magnitude = math.sqrt(vector[0]**2 + vector[1]**2 + vector[2]**2)
+        rest_magnitude = math.sqrt(1**2 + 0**2 + 0**2)
+        angle = math.acos(dot_product / (vector_magnitude * rest_magnitude))
+        angle = math.degrees(angle)
+
+        # check if the angle is within the FOV ranges
+        if angle <= 100: # horizontal range is +/- 100 degrees from the rest position
+            if angle <= 75: # vertical range is +/- 75 degrees from the rest position
+                print("Feasible vector")
+                return True
+            else:
+                print("Not a feasible vector")
+                return False
+        else:
+            print("Not a feasible vector")
+            return False
+
     def onCallLook(self):
         """Lets NAO look to a certain Position"""
 
-        try:
-            self.tracker.lookAt([self.x, self.y, self.z], self.frame, self.maxSpeed, self.useWholeBody)
-        except Exception as excpt:
-            print(excpt)
+        valid = self.check_FOV_limits(np.array([self.x, self.y, self.z]))
+        if valid:
+            try:
+                self.tracker.lookAt([self.x, self.y, self.z], self.frame, self.maxSpeed, self.useWholeBody)
+            except Exception as excpt:
+                print(excpt)
+        else:
+            return
 
     def onCallPoint(self):
 
     # TODO add decision mechanic for arm to point with
+        if self.y > 0:
+            self.effector = "LArm"
+        else:
+            self.effector = "RArm"
         try:
             self.tracker.pointAt(self.effector, [self.x, self.y, self.z], self.frame, self.maxSpeed)
         except Exception as excpt:
             print(excpt)       
 
     def onCallSay(self, text):
+        """
+        Lets NAO say the given text.
+
+        :param text: The text for NAO to say.
+        """
+
         try:
             self.tts.say(text)
         except Exception as excpt:
             print(excpt)     
 
-    def back_to_init(self):
-        """Code Snippet from Choregraphe
-            Lets NAO move back to its original posture
-            This posture can easily be defined using choregraphs timeline box
-            A defined posture there can be exported as bezier style python code
-        """
-        # Go back to initial Pose, to prevent unwanted behavior
-        # Choregraphe bezier export in Python.
-
-        names = list()
-        times = list()
-        keys = list()
-
-        names.append("HeadPitch")
-        times.append([1.2])
-        keys.append([[-0.0153821, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("HeadYaw")
-        times.append([1.2])
-        keys.append([[-0.021518, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LAnklePitch")
-        times.append([1.2])
-        keys.append([[0.667248, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LAnkleRoll")
-        times.append([1.2])
-        keys.append([[0.0767419, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LElbowRoll")
-        times.append([1.2])
-        keys.append([[-1.20568, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LElbowYaw")
-        times.append([1.2])
-        keys.append([[-0.490922, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LHand")
-        times.append([1.2])
-        keys.append([[0.0104001, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LHipPitch")
-        times.append([1.2])
-        keys.append([[-1.26551, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LHipRoll")
-        times.append([1.2])
-        keys.append([[0.34826, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LHipYawPitch")
-        times.append([1.2])
-        keys.append([[-0.638102, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LKneePitch")
-        times.append([1.2])
-        keys.append([[1.50021, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LShoulderPitch")
-        times.append([1.2])
-        keys.append([[0.918824, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LShoulderRoll")
-        times.append([1.2])
-        keys.append([[0.28835, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("LWristYaw")
-        times.append([1.2])
-        keys.append([[0.0429101, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RAnklePitch")
-        times.append([1.2])
-        keys.append([[0.673468, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RAnkleRoll")
-        times.append([1.2])
-        keys.append([[-0.0214341, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RElbowRoll")
-        times.append([1.2])
-        keys.append([[1.29934, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RElbowYaw")
-        times.append([1.2])
-        keys.append([[0.454022, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RHand")
-        times.append([1.2])
-        keys.append([[0.0388, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RHipPitch")
-        times.append([1.2])
-        keys.append([[-1.21804, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RHipRoll")
-        times.append([1.2])
-        keys.append([[-0.315962, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RHipYawPitch")
-        times.append([1.2])
-        keys.append([[-0.638102, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RKneePitch")
-        times.append([1.2])
-        keys.append([[1.47422, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RShoulderPitch")
-        times.append([1.2])
-        keys.append([[0.91584, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RShoulderRoll")
-        times.append([1.2])
-        keys.append([[-0.339056, [3, -0.4, 0], [3, 0, 0]]])
-
-        names.append("RWristYaw")
-        times.append([1.2])
-        keys.append([[-0.044528, [3, -0.4, 0], [3, 0, 0]]])
-
-        try:
-          # uncomment the following line and modify the IP if you use this script outside Choregraphe.
-          # motion = ALProxy("ALMotion", IP, 9559)
-          motion = ALProxy("ALMotion")
-          motion.angleInterpolationBezier(names, times, keys)
-        except BaseException as err:
-          print(err)
-
     def flash_eyes(self, color=None):
+        """
+        Flashes the robot's eyes with the given color.
+
+        :param color: The color to flash the robot's eyes with.
+        """
 
         sGroup = "FaceLeds"
 
@@ -368,8 +341,17 @@ class CommandExecuterModule(ALModule):
 
 # Main Function for NaoPosner Experiment
 class ArmodIntegrationClass():
+    """
+    This class holds the primary functionalties for the ARMoD experiment.
+    """
 
     def __init__(self, naoip, naoport):
+        """
+        Initializes a new instance of the ArmodIntegrationClass class.
+
+        :param naoip: The IP address of the NAO robot.
+        :param naoport: The port number of the NAO robot.
+        """
 
         global config
 
@@ -395,7 +377,10 @@ class ArmodIntegrationClass():
 
         self.rest_pose = rp
 
-    def nao_rest(self):
+    def nao_head_rest(self):
+        """
+        Sets the NAO robots Head to its resting position.
+        """
         self.CommandExecuter.tracker.lookAt([self.rest_pose["x"], self.rest_pose["y"], self.rest_pose["z"]], 0, self.CommandExecuter.maxSpeed, False)
         self.CommandExecuter.resting = True
 
@@ -403,30 +388,33 @@ if __name__ == '__main__':
     # # Start a ROS node that will run in parallel to the main loop
     # rospy.init_node('my_node')
 
+    # Create a new instance of the ArmodIntegrationClass with the given NAO IP and port (Config.ini)
     e = ArmodIntegrationClass(NAO_IP, NAO_PORT)
 
+    # If debug mode is enabled (Config.ini)
+    # Test implementation
     if e.DEBUG:
         print("NAO will look somewhere ...")
         e.CommandExecuter.updateCoordinates(2, 1, 3)
         e.CommandExecuter.onCallLook()
         time.sleep(3)
         print("Bring NAO back to resting position ...")
-        e.nao_rest()
+        e.nao_head_rest()
 
         print("Let NAO point somewhere ...")
         e.CommandExecuter.updateCoordinates(2, 1, 3)
         e.CommandExecuter.onCallPoint()
         time.sleep(3)
-        e.CommandExecuter.back_to_init()
+        e.CommandExecuter.posture.goToPosture("Sit", 0.8)
 
         print("Let NAO say something ...")
         e.CommandExecuter.onCallSay("Hello there!")
 
     try:
+        # Keep running until the exit flag is set
         while not e.CommandExecuter.exit_flag:
-            #print("Testing ROS ...")
-            #time.sleep(1)
             pass
     finally:
+        # Shutdown the broker and exit
         e.myBroker.shutdown()
         sys.exit(0)
