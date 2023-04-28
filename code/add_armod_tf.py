332ad5baa8ab59b79280cca5e0e8549683c88c36
Tim Schreiter <tim.schreiter@oru.se>
2023-04-28 16:45:44 +0200

    Updated Nao TF Frame Broadcaster

--- code/add_armod_tf.py
+++ code/add_armod_tf.py
@@ -2,24 +2,34 @@
 
 import rospy
 import tf
+from std_msgs.msg import String
 
-def main():
-    rospy.init_node('nao_tf_broadcaster')
-    br = tf.TransformBroadcaster()
-    br2 = tf.TransformBroadcaster()
-    listener = tf.TransformListener()
-    rate = rospy.Rate(90.0)
-    while not rospy.is_shutdown():
-        now = rospy.Time.now()
-        br.sendTransform(
-                # Use this if X-Axis is aligned with robot_base_foorprint
-            (0.3, 0.0, 0.85),
-            # Use remaped base_footprint as parent frame
-            (0.00000000e+00,   0.00000000e+00,   0.00000000e+00,  1.0),
-            now,
-            "/robot_armod_frame",
-            "/robot_base_footprint")
-        rate.sleep()
+class ArmodFrameClass:
+    def __init__(self):
+        rospy.init_node('nao_tf_broadcaster')
+        self.br = tf.TransformBroadcaster()
+        self.br2 = tf.TransformBroadcaster()
+        self.listener = tf.TransformListener()
+        rospy.Subscriber("armod_orientation", String, self.callback)
+        self.rate = rospy.Rate(90.0)
+
+    def callback(self, data):
+        msg = data.data.split(",")
+        self.q = [msg[1], msg[2], msg[3], msg[4]]
+
+    def update_static_transform_broadcaster(self):
+        while not rospy.is_shutdown():
+            now = rospy.Time.now()
+            self.br.sendTransform(
+                    # Use this if X-Axis is aligned with robot_base_foorprint
+                (0.3, 0.0, 0.85),
+                # Use remaped base_footprint as parent frame
+                (self.q[0],   self.q[1],   self.q[2],  self.q[3]),
+                now,
+                "/robot_armod_frame",
+                "/robot_base_footprint")
+            self.rate.sleep()
 
 if __name__ == '__main__':
-    main()
+    armod_frame_class = ArmodFrameClass()
+    armod_frame_class.update_static_transform_broadcaster()
